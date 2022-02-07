5c8485a2c92391b73dcb2d8f9ad6a772a927a3cb
Anthony Poon <83668891+anthonypoon12@users.noreply.github.com>
2022-02-07 01:47:52 -0500

    Worked on a bit of Assignment 9

---
+++ Assignment_9.py
@@ -0,0 +1,17 @@
+myList = {1,2,3,4}
+myList2={}
+for i in range(1,myList.length):
+    myList2+=List[i]
+myList2+="appendeditem"
+myList2.pop(2)
+myList3 = myList2
+
+#count(sub,[start,[end]]) shows the number of times the substring sub appears in the range [start,end]
+# endswith(suffix,[start,[end]]) returns True if the string ends with suffix from the indicies [start,end]
+# find/index(sub,[start,[end]]) if the substring exists inside the string, finds the lowest index where the substring is found from indicies [start,end]
+# join() takes all elements in paramters and joins them in one string
+# replace(old,new[,count]) returns a copy of the string with the "old" portion of the string replafce with "new". If count is included, replace only happens count times maximum
+# split([sep[,maxsplit]])
+# splitlines([keepends])
+# startswith(prefix[,start[,end]])
+# strip([chars])
